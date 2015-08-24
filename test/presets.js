fc6c48800bab1cbdfea0845fbfa93a2583d61347
Javier Cejudo <javier@javiercejudo.com>
2015-08-24 23:38:30 +1000

    feat: modify presets schema

--- test/presets.js
+++ test/presets.js
@@ -5,7 +5,7 @@
 require('should');
 
 var rescale = require('rescale')(require('floating-adapter')).rescale;
-var length = require('../src/linear-presets-length');
+var length = require('linear-preset-factory')(require('../src/linear-presets-length'));
 
 function convert(x, preset) {
   return rescale(x, preset[0], preset[1]);
@@ -17,22 +17,22 @@ function invert(preset) {
 
 describe('temperature presets', function() {
   it('should convert correctly', function() {
-    (42195).should.be.exactly(convert(42.195, invert(length.metreToKilometre)), 'metreToKilometre')
-      .and.exactly(convert(4219500, invert(length.metreToCentimetre)), 'metreToCentimetre')
-      .and.exactly(convert(42195000, invert(length.metreToMillimetre)), 'metreToMillimetre')
-      .and.exactly(convert(26.218757456454306, invert(length.metreToMile)), 'metreToMile')
-      .and.exactly(convert(46145.01312335958, invert(length.metreToYard)), 'metreToYard')
-      .and.exactly(convert(138435.03937007874, invert(length.metreToFoot)), 'metreToFoot')
-      .and.exactly(convert(1661220.472440945, invert(length.metreToInch)), 'metreToInch')
-      .and.exactly(convert(22.783477321814257, invert(length.metreToNauticalMile)), 'metreToNauticalMile');
+    (42195).should.be.exactly(convert(42.195, invert(length.metre_kilometre)), 'metre_kilometre')
+      .and.exactly(convert(4219500, invert(length.metre_centimetre)), 'metre_centimetre')
+      .and.exactly(convert(42195000, invert(length.metre_millimetre)), 'metre_millimetre')
+      .and.exactly(convert(26.218757456454306, invert(length.metre_mile)), 'metre_mile')
+      .and.exactly(convert(46145.01312335958, invert(length.metre_yard)), 'metre_yard')
+      .and.exactly(convert(138435.03937007874, invert(length.metre_foot)), 'metre_foot')
+      .and.exactly(convert(1661220.472440945, invert(length.metre_inch)), 'metre_inch')
+      .and.exactly(convert(22.783477321814257, invert(length.metre_nauticalMile)), 'metre_nauticalMile');
 
-    (0).should.be.exactly(convert(0, length.metreToKilometre), 'metreToKilometre')
-      .and.exactly(convert(0, length.metreToCentimetre), 'metreToCentimetre')
-      .and.exactly(convert(0, length.metreToMillimetre), 'metreToMillimetre')
-      .and.exactly(convert(0, length.metreToMile), 'metreToMile')
-      .and.exactly(convert(0, length.metreToYard), 'metreToYard')
-      .and.exactly(convert(0, length.metreToFoot), 'metreToFoot')
-      .and.exactly(convert(0, length.metreToInch), 'metreToInch')
-      .and.exactly(convert(0, length.metreToNauticalMile), 'metreToNauticalMile');
+    (0).should.be.exactly(convert(0, length.metre_kilometre), 'metre_kilometre')
+      .and.exactly(convert(0, length.metre_centimetre), 'metre_centimetre')
+      .and.exactly(convert(0, length.metre_millimetre), 'metre_millimetre')
+      .and.exactly(convert(0, length.metre_mile), 'metre_mile')
+      .and.exactly(convert(0, length.metre_yard), 'metre_yard')
+      .and.exactly(convert(0, length.metre_foot), 'metre_foot')
+      .and.exactly(convert(0, length.metre_inch), 'metre_inch')
+      .and.exactly(convert(0, length.metre_nauticalMile), 'metre_nauticalMile');
   });
 });
